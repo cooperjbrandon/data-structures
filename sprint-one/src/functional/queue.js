41dfe05235fa1918ea766d4e82aa527fa5c31e4e
Hack Reactor Students <cooperjbrandon@gmail.com>
2013-11-13 15:58:04 -0800

    Completed Functional Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var lastRemoved = -1;
+  var orderInLine = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[orderInLine] = value;
+    orderInLine++;
+    size++;
   };
 
   instance.dequeue = function(){
+    size && size--;
+    lastRemoved++;
+    return storage[lastRemoved];
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
