269a502e6f3292c55cddaf6117b1b180c1d9f979
Hack Reactor Students <cooperjbrandon@gmail.com>
2013-11-13 15:41:31 -0800

    completed Function Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,18 +3,22 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   instance.pop = function(){
+    size && size--;
+    return storage[size];
   };
 
   instance.size = function(){
-    return 0;
+    return size;
   };
 
   return instance;
