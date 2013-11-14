d7a7491a416487595f71b793b8df9340c8450090
Hack Reactor Students <cooperjbrandon@gmail.com>
2013-11-14 15:55:29 -0800

    Finished Pseudoclassical queue implementation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,24 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+
+ this.storage = {};
+  this.length = 0;
+  this.lastRemoved = -1;
+  this.orderAdded = 0;
 };
+
+
+  Queue.prototype.enqueue = function(value){
+    this.storage[this.orderAdded] = value;
+    this.orderAdded++;
+    this.length++;
+  };
+
+  Queue.prototype.dequeue = function(){
+    this.length && this.length--;
+    this.lastRemoved++;
+    return this.storage[this.lastRemoved];
+  };
+
+  Queue.prototype.size = function(){
+    return this.length;
+  };
