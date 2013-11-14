1413369c4292b577f6dac68c3175ce7367f843a8
Hack Reactor Students <cooperjbrandon@gmail.com>
2013-11-14 10:57:51 -0800

    Finished first draft of linkedList, updated spec file

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -16,5 +16,29 @@ describe("linkedList", function() {
     expect(linkedList.contains).toEqual(jasmine.any(Function));
   });
 
+  // it("should return the first node of the list when .removeHead is called", function(){
+  //   expect(linkedList.)
+  // });
+
+  // it('does not error when removing from an empty queue', function() {
+  //   expect(function(){ linkedList.removeHead(); }).not.throws();
+  // });
+
+  it('allows sequentially adding and removing items', function() {
+    linkedList.addToTail('a');
+    expect(linkedList.removeHead()).toEqual('a');
+    linkedList.addToTail('b');
+    expect(linkedList.removeHead()).toEqual('b');
+  });
+
+  it('points to correct head when sequentially adding and removing items', function() {
+    linkedList.addToTail('a');
+    expect(linkedList.head).toEqual('a');
+    linkedList.addToTail('b');
+    linkedList.addToTail('c');
+    linkedList.removeHead();
+    expect(linkedList.head).toEqual('b');
+  });
+
   // add more tests here to test the functionality of linkedList
 });
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,24 +1,48 @@
 // Note: don't use an array to do this.
 var makeLinkedList = function(){
   var list = {};
+  var storage = {};
+  var order = 0;
+  var lastRemoved = -1;
+  var size = 0;
+  var containsObject = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(){
+  list.addToTail = function(num){
+    storage[order] = makeNode(num); //order = 0; storage.0 = newnode1; storage.0.value = num; storage.0.next = null
+    // second call: order = 1; storage.1 = newnode2; storage.1.value = num2; storage.1.next = null;
+    containsObject[storage[order].value] = true;
+    if(size > 0) {
+      storage[order-1].next = storage[order];
+    } else {
+      list.head = storage[order].value;
+    }
+    list.tail = storage[order].value;
+    // storage.0.value = storage.1;
+    order++; // order = 1
+    size++; // size = 1;
   };
 
   list.removeHead = function(){
-  };
+    if(size) {
+      size--;
+      lastRemoved++;
+      list.head = storage[lastRemoved+1].value;
+      delete containsObject.lastRemoved;
+      return storage[lastRemoved].value;
+    }};
 
-  list.contains = function(){
+  list.contains = function(num){
+    return _.contains(Object.keys(containsObject), num);
   };
 
   return list;
 };
 
-var makeNode = function(value){
+var makeNode = function(num){
   var node = {};
-  node.value = value;
+  node.value = num;
   node.next = null;
 
   return node;
