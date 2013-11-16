eb9ecdf819b43f2b2f109bed3134755ea1868912
Hack Reactor Students <cooperjbrandon@gmail.com>
2013-11-16 12:33:00 -0800

    Updated BSTree and BSTree spec for left right and insert

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -1,14 +1,46 @@
 describe("binarySearchTree", function() {
   var binarySearchTree;
+  var firstNode;
+  var secondNode;
+  var thirdNode;
+  var fifthNode;
+  var fourthNode;
 
   beforeEach(function() {
-    binarySearchTree = makeBinarySearchTree();
+    binarySearchTree = makeBinarySearchTree('Root');
+    firstNode = makeBinarySearchTree(4);
+    secondNode = makeBinarySearchTree(3);
+    binarySearchTree.insert(firstNode);
+    binarySearchTree.insert(secondNode);
+    thirdNode = makeBinarySearchTree(5);
+    fourthNode = makeBinarySearchTree(9);
+    fifthNode = makeBinarySearchTree(8);
+    firstNode.insert(thirdNode);
+    secondNode.insert(fourthNode);
+    secondNode.insert(fifthNode);
+    console.log(binarySearchTree);
   });
 
   it("should have methods named 'insert', 'contains', and 'depthFirstLog", function() {
     expect(binarySearchTree.insert).toEqual(jasmine.any(Function));
     expect(binarySearchTree.contains).toEqual(jasmine.any(Function));
     expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
+  });
+
+  it("should be able to tell if a given node contains the passed in value for insert", function() {
+    expect(binarySearchTree.contains(4)).toEqual(true);
+    expect(binarySearchTree.contains('asd')).toEqual(false);
+    expect(binarySearchTree.contains(9)).toEqual(true);
+    expect(binarySearchTree.contains(5)).toEqual(true);
+  });
+
+  it("should be able to tell if a given node contains the passed in value for removeParent", function() {
+    secondNode.removeParent();
+    expect(fourthNode.parent.parent).toEqual(null);
+    expect(binarySearchTree.contains(3)).toEqual(false);
+    expect(secondNode.contains(8)).toEqual(true);
+    expect(binarySearchTree.contains(4)).toEqual(true);
+    expect(binarySearchTree.contains(9)).toEqual(false);
   });
   // add more tests here to test the functionality of binarySearchTree
 });
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,74 @@
-var makeBinarySearchTree = function(){
+var makeBinarySearchTree = function(value){
+  var newBSTree = {};
+  newBSTree.value = value;
+  newBSTree.children = [];
+  newBSTree.parent = null;
+  newBSTree.insert = bsTreeMethods.insert;
+  newBSTree.contains = bsTreeMethods.contains;
+  newBSTree.removeParent = bsTreeMethods.removeParent;
+  newBSTree.depthFirstLog = bsTreeMethods.depthFirstLog;
+  newBSTree.left = null;
+  newBSTree.right = null;
+  return newBSTree;
 };
+
+var bsTreeMethods = {};
+
+bsTreeMethods.insert = function(node){
+  node.parent = this;
+  var childSize = this.children.length;
+  if (childSize > 0 && node.value >= this.children[childSize - 1].value) {
+    node.left = this.children[childSize - 1];
+    this.children[childSize - 1].right = node;
+    this.children.push(node);
+  } else {
+    var result = 0;
+    var keepGoing = true;
+    for (var i = 0; i < this.children.length; i++) {
+      if (keepGoing && node.value < this.children[i].value) {
+        result = i;
+        keepGoing = false;
+      }
+    }
+    this.children.splice(result, 0 , node);
+    if (this.children.length !== 1) {
+      node.right = this.children[result + 1];
+      this.children[result+1].left = node;
+    }
+    if (result !== 0) {
+      node.left = this.children[result - 1];
+      this.children[result- 1].right = node;
+    }
+  }
+};
+
+bsTreeMethods.removeParent = function(){
+  for (var i = 0; i < this.parent.children.length; i++) {
+    if (this.parent.children[i] === this){
+      this.parent.children.splice(i, 1);
+    }
+  }
+  this.parent = null;
+};
+
+bsTreeMethods.contains = function(value){
+  var child;
+  var temp;
+  if (this.value === value) {
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++){
+      child = this.children[i];
+      temp = child.contains(value);
+      if (temp !== false){
+        return temp;
+      }
+    }
+  }
+  return false;
+};
+
+bsTreeMethods.depthFirstLog = function(node){
+
+};
+
